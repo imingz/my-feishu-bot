fcc6437fb54b45b6ebc3c777f6dc35bf8eccdbad
imingz <imingz@qq.com>
2024-06-18 10:52:08 +0800

    feat: 添加 qrcode 功能和发送图片消息的函数

---
+++ internal/biz/qrcode/qrcode.go
@@ -0,0 +1,19 @@
+package qrcode
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"xiaoxiaojiqiren/internal/pkg/qrcodeclient"
+
+	"github.com/skip2/go-qrcode"
+)
+
+func getQrcodeFile() (io.Reader, error) {
+	gotQrcode, err := qrcodeclient.GetQrcode(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	content, err := qrcode.Encode(gotQrcode, qrcode.Medium, 256)
+	return bytes.NewReader(content), err
+}
