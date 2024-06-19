67f325d86873a32ce2956f9e2b5baf1c900a8ba9
imingz <imingz@qq.com>
2024-06-19 12:26:16 +0800

    feat<门禁二维码>: 实现 复用卡片消息

--- internal/pkg/biz/qrcode_card.go
+++ internal/pkg/biz/qrcode_card.go
@@ -7,25 +7,35 @@ import (
 	"io"
 	"time"
 	"xiaoxiaojiqiren/internal/pkg/client"
-	"xiaoxiaojiqiren/internal/pkg/config"
 	"xiaoxiaojiqiren/internal/pkg/qrcode"
 
+	larkcard "github.com/larksuite/oapi-sdk-go/v3/card"
 	larkim "github.com/larksuite/oapi-sdk-go/v3/service/im/v1"
 )
 
-func SendQrcodeCard(messageId string) error {
+func GenerateQrcodeCard(messageId string) (*larkcard.MessageCard, error) {
 	// 1. 获取二维码图片
 	img, err := qrcode.GetQrcodeFile()
 	if err != nil {
-		return errors.Join(err, errors.New("获取二维码图片失败"))
+		return nil, errors.Join(err, errors.New("获取二维码图片失败"))
 	}
 	// 2. 上传图片
 	imageKey, err := uploadImage(img)
 	if err != nil {
+		return nil, err
+	}
+	// 3. 获取消息卡片
+	return getQrcodeCard(imageKey), nil
+}
+
+func SendQrcodeCard(messageId string) error {
+	// 1. 生成卡片消息
+	card, err := GenerateQrcodeCard(messageId)
+	if err != nil {
 		return err
 	}
-	// 3. 发送卡片消息
-	return sendQrcodeCard(messageId, imageKey)
+	// 2. 发送卡片消息
+	return sendQrcodeCard(messageId, card)
 }
 
 // 上传图片，返回图片key
@@ -56,15 +66,60 @@ func uploadImage(image io.Reader) (string, error) {
 	return *resp.Data.ImageKey, nil
 }
 
+// 获取卡片
+func getQrcodeCard(imageKey string) *larkcard.MessageCard {
+	// header
+	header := larkcard.NewMessageCardHeader().
+		Template(larkcard.TemplateWathet).
+		Title(larkcard.NewMessageCardPlainText().
+			Content(time.Now().Format(time.DateTime)).
+			Build()).
+		Build()
+
+	// 二维码图片
+	qrcodeElement := larkcard.NewMessageCardImage().
+		ImgKey(imageKey).
+		Alt(larkcard.NewMessageCardPlainText().Content("图片").Build()).
+		CompactWidth(true).
+		Build()
+
+	// 重新生成按钮
+	generateButton := larkcard.NewMessageCardAction().
+		Actions([]larkcard.MessageCardActionElement{
+			larkcard.NewMessageCardEmbedButton().
+				Type(larkcard.MessageCardButtonTypePrimary).
+				Text(larkcard.NewMessageCardPlainText().
+					Content("重新生成").
+					Build()).
+				Value(map[string]any{
+					"qrcode": "refresh",
+				}).
+				Build(),
+		}).
+		Build()
+
+	// 卡片消息体
+	messageCard := larkcard.NewMessageCard().
+		Header(header).
+		Elements([]larkcard.MessageCardElement{qrcodeElement, generateButton}).
+		Build()
+
+	return messageCard
+}
+
 // 发送二维码卡片消息
-func sendQrcodeCard(messageId, imageKey string) error {
+func sendQrcodeCard(messageId string, card *larkcard.MessageCard) error {
 	// 错误预定义
 	var sendQrcodeCardErr = errors.New("发送二维码卡片消息失败")
 	// 创建请求对象
+	cardJson, err := card.JSON()
+	if err != nil {
+		return errors.Join(err, sendQrcodeCardErr)
+	}
 	req := larkim.NewReplyMessageReqBuilder().
 		MessageId(messageId).
 		Body(larkim.NewReplyMessageReqBodyBuilder().
-			Content(`{"type": "template", "data": { "template_id": "` + config.Get().Qrcode.CardId + `", "template_variable": {"time": "` + time.Now().Format(time.DateTime) + `", "imgKey": "` + imageKey + `"} } }`).
+			Content(cardJson).
 			MsgType(`interactive`).
 			ReplyInThread(true).
 			Build()).
--- internal/pkg/httpclient/handler/qrcodeCardHandler.go
+++ internal/pkg/httpclient/handler/qrcodeCardHandler.go
@@ -2,21 +2,17 @@ package handler
 
 import (
 	"context"
-	"fmt"
 	"xiaoxiaojiqiren/internal/pkg/biz"
 	"xiaoxiaojiqiren/internal/pkg/config"
 
 	larkcard "github.com/larksuite/oapi-sdk-go/v3/card"
-	larkcore "github.com/larksuite/oapi-sdk-go/v3/core"
 )
 
 var CardHandler = larkcard.NewCardActionHandler(config.Get().APP.VerificationToken, config.Get().APP.EventEncryptKey, qrcodeCardHandler)
 
 func qrcodeCardHandler(ctx context.Context, cardAction *larkcard.CardAction) (any, error) {
-	fmt.Println(larkcore.Prettify(cardAction))
 	if cardAction.Action.Value["qrcode"] == "refresh" {
-		err := biz.SendQrcodeCard(cardAction.OpenMessageID)
-		return nil, err
+		return biz.GenerateQrcodeCard(cardAction.OpenMessageID)
 	}
 	return nil, nil
 }
