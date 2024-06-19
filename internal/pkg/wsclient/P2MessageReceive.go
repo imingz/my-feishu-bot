ae7f4c0820dffbcd5e55a3ca369379938636f63a
imingz <imingz@qq.com>
2024-06-19 22:56:50 +0800

    chore: Rename P2MessageReceive.go to wsclient/P2MessageReceive.go and remove event_handler.go file

--- internal/pkg/wsclient/P2MessageReceive.go
+++ internal/pkg/wsclient/P2MessageReceive.go
@@ -1,4 +1,4 @@
-package handler
+package wsclient
 
 import (
 	"context"
--- internal/pkg/wsclient/wsclient.go
+++ internal/pkg/wsclient/wsclient.go
@@ -4,27 +4,29 @@ import (
 	"sync"
 	"xiaoxiaojiqiren/internal/pkg/config"
 	"xiaoxiaojiqiren/internal/pkg/env"
-	"xiaoxiaojiqiren/internal/pkg/wsclient/handler"
 
 	larkcore "github.com/larksuite/oapi-sdk-go/v3/core"
+	"github.com/larksuite/oapi-sdk-go/v3/event/dispatcher"
 	larkws "github.com/larksuite/oapi-sdk-go/v3/ws"
 )
 
 var client *larkws.Client
-
 var once sync.Once
+var handler *dispatcher.EventDispatcher
 
 func Get() *larkws.Client {
 	once.Do(func() {
+		handler = dispatcher.NewEventDispatcher("", "").
+			OnP2MessageReceiveV1(P2MessageReceive)
 		switch env.Active {
 		case env.DEV:
 			client = larkws.NewClient(config.Get().APP.ID, config.Get().APP.Secret,
-				larkws.WithEventHandler(handler.Get()),
+				larkws.WithEventHandler(handler),
 				larkws.WithLogLevel(larkcore.LogLevelDebug),
 			)
 		case env.PRO:
 			client = larkws.NewClient(config.Get().APP.ID, config.Get().APP.Secret,
-				larkws.WithEventHandler(handler.Get()),
+				larkws.WithEventHandler(handler),
 				larkws.WithLogLevel(larkcore.LogLevelInfo),
 			)
 		}
